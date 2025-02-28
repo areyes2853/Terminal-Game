4494ba80bde1f92db802b4a0e4cde434f697f8b1
areyes2853 <46231827+areyes2853@users.noreply.github.com>
2025-02-28 15:47:58 -0500

    determineWinnner fuction done

--- app.js
+++ app.js
@@ -43,16 +43,39 @@ function getComputerMove() {
   // Function to determine the winner given a player's move and the computer's move
   function determineWinner(playerMove, computerMove) {
     // If the moves are the same, it's a tie
+    let winner;
+   if(playerMove === computerMove){
+    winner = "tie"
+   }
+   else if(playerMove === "rock"&& computerMove === "scissors"){
+    winner = 'player'
+   }
+   else if(playerMove === "paper" && computerMove ==="rock"){
+    winner = 'player'
+   }
+   else if(playerMove=== "scissors" && computerMove=== "paper"){
+    winner = 'player'
+   }
+   else{
+    //winner = `Computer wins ${username}`
+    winner ='computer'
+   }
+   return winner;
     // Else if playerMove is 'rock' and computerMove is 'scissors', player wins
     // Else if playerMove is 'paper' and computerMove is 'rock', player wins
     // Else if playerMove is 'scissors' and computerMove is 'paper', player wins
     // Else, the computer wins
     // Return the result (e.g., 'win', 'lose', 'tie')
   }
+  let computerMove = getComputerMove();
+  console.log(determineWinner("rock",computerMove))
+  
   
   // Main function to play the game
   function playGame() {
+    
     // Prompt the user to enter their move (e.g., 'rock', 'paper', or 'scissors')
+    const playerMove = prompt('What is your move? ');
     // Validate the input (ensure it's one of the allowed moves)
     // Call getComputerMove() to obtain the computer's move
     // Call determineWinner(playerMove, computerMove) to decide the outcome
